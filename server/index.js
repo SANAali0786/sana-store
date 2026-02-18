76a313744a841f49286ea630466c15e8dc6c3011
Sana <praveensajiya01@gmail.com>
2026-02-18 09:15:04 +0530

    fix env for production

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ const pgSession = require('connect-pg-simple')(session);
 const pool = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 6543;
+const PORT = process.env.PORT || 5000;
 
 // --- 1. MIDDLEWARES ---
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://sana-store.vercel.app/" ],
+  origin: ["http://localhost:5173", "https://sana-store.vercel.app" ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
